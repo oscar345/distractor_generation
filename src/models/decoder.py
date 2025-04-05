44f609af46a6c30439dae6105b42e950a1918b84
Oscar Zwagers <o.zwagers@icloud.com>
2025-04-05 12:14:35 +0200

    Add llama model training and prediction functions
    - Implement `train` function to train the llama model
    - Implement `predict` function to generate predictions using the trained model
    - Refactor `tokenize` function to `tokenize_function` with `with_labels` parameter
    - Add utility functions `load_in_4bit`, `safe_get`, and `generate_predictions`

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -2,61 +2,103 @@
 # is the tokenizer function. Since that code is shared, it is stored in a
 # separate file.
 
-from typing import Dict
-from transformers import AutoTokenizer
+from typing import Dict, List, Any
+from datasets import Dataset
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, default_data_collator
+from torch.utils.data import DataLoader
+import re
+
+from utils import Distractors
 
 
-def tokenize(tokenizer: AutoTokenizer, examples: Dict, training: bool):
-    """Tokenize the input text and target text when training. When not training, only
-    tokenize the input text, since the target text is not available."""
-
+def tokenize_function(tokenizer: AutoTokenizer, examples: Dict, with_labels=True):
+    """"""
     text = (
-        [
-            text + tokenizer.eos_token + target + tokenizer.eos_token  # type: ignore
-            for text, target in zip(examples["text"], examples["target"])
-        ]
-        if training
-        else [
-            text + tokenizer.eos_token  # type: ignore
-            for text in examples["text"]
-        ]
+        [text + target for text, target in zip(examples["text"], examples["target"])]
+        if with_labels
+        else examples["text"]
     )
 
     tokenized = tokenizer(
         text,
         padding="max_length",
         truncation=True,
-        max_length=768 + 64 + 2,
+        max_length=768 + 64,
         return_tensors="pt",
     )  # type: ignore
 
-    if not training:
+    if with_labels:
         return {
+            "labels": tokenized["input_ids"].clone(),
             "input_ids": tokenized["input_ids"],
             "attention_mask": tokenized["attention_mask"],
         }
 
-    sep_token_id = tokenizer.eos_token_id  # type: ignore
-    input_ids = tokenized["input_ids"]
-
-    # Find the second EOS (start of the target)
-    labels = input_ids.clone()
-
-    for i, ids in enumerate(input_ids):
-        eos_indices = (ids == sep_token_id).nonzero(as_tuple=True)[0]
-
-        if len(eos_indices) < 2:
-            # If there's no second EOS, mask everything
-            labels[i, :] = -100
-        else:
-            # Mask everything before the second EOS (including the second EOS itself).
-            # Because of this the loss will only be calculated based on the other part
-            # of the text the model generated (which is the target, and thus the
-            # distractors).
-            labels[i, : eos_indices[1] + 1] = -100
-
     return {
-        "input_ids": input_ids,
+        "input_ids": tokenized["input_ids"],
         "attention_mask": tokenized["attention_mask"],
-        "labels": labels,
     }
+
+
+def load_in_4bit(device: torch.device) -> bool:
+    return device == "cuda"
+
+
+def safe_get(list: List[Any], index: int) -> Any:
+    l = len(list)
+
+    if index < l:
+        return list[index]
+    else:
+        return None
+
+
+def generate_predictions(
+    model: AutoModelForCausalLM,
+    dataset: Dataset,
+    tokenizer: AutoTokenizer,
+    device: torch.device,
+) -> List[Distractors]:
+    dataloader = DataLoader(
+        dataset,  # type:ignore
+        batch_size=4,
+        shuffle=False,
+        collate_fn=default_data_collator,
+    )
+
+    model.eval()  # type:ignore set the model in evaluation mode
+
+    outputs = []
+
+    for batch in dataloader:
+        # set input to the correct device
+        batch = {k: v.to(device) for k, v in batch.items()}
+
+        with torch.no_grad():
+            generated = model.generate(  # type: ignore
+                input_ids=batch["input_ids"],
+                attention_mask=batch["attention_mask"],
+                max_new_tokens=64,
+                do_sample=True,
+                top_k=50,
+            )
+
+        text = tokenizer.batch_decode(generated, skip_special_tokens=True)  # type:ignore
+        outputs.extend(text)
+
+    pattern = r"Distractor \d+: (.+)"
+    distractors = []
+
+    for output in outputs:
+        answers = re.findall(pattern, output)
+
+        distractor = Distractors(
+            distractor1=safe_get(answers, 0),
+            distractor2=safe_get(answers, 1),
+            distractor3=safe_get(answers, 2),
+        )
+
+        distractors.append(distractor)
+
+    return distractors
---
+++ src/models/llama.py
@@ -0,0 +1,96 @@
+from typing import cast
+from datasets import Dataset, load_dataset
+from peft import LoraConfig, get_peft_model
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
+from models import decoder
+from options import Config
+import utils
+
+
+def train(config: Config):
+    dataset = utils.load_dataset_from_disk(config)
+    tokenizer = AutoTokenizer.from_pretrained(config.llama_model_name)
+    device = utils.get_device()
+
+    dataset = dataset.map(
+        lambda examples: decoder.tokenize_function(tokenizer, examples), batched=True
+    )
+
+    peft_config = LoraConfig(
+        r=8,
+        lora_alpha=32,
+        target_modules=["q_proj", "v_proj"],
+        lora_dropout=0.05,
+        bias="none",
+    )
+
+    print("Loading model...")
+    model = AutoModelForCausalLM.from_pretrained(
+        config.llama_model_name,
+        load_in_4bit=decoder.load_in_4bit(device),
+        device_map="auto",
+        torch_dtype=torch.float16,
+    )
+    print("Model loaded")
+    model = get_peft_model(model, peft_config)
+    model = model.to(device)
+
+    training_args = TrainingArguments(
+        output_dir=utils.get_results_directory_name(config),
+        eval_strategy="epoch",
+        per_device_eval_batch_size=4,
+        per_device_train_batch_size=4,
+        num_train_epochs=3,
+        learning_rate=5e-5,
+        bf16=True,
+        save_strategy="steps",
+        save_total_limit=2,
+        save_steps=350,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["evaluation"],
+    )
+
+    print("Training the model")
+    trainer.train()
+
+    print("Saving the model...")
+    model.save_pretrained(utils.get_model_directory_name(config))
+    print("Model saved")
+
+
+def predict(config: Config):
+    print("Loading dataset...")
+    test_dataset = cast(Dataset, load_dataset(config.dataset_name, split="test"))
+    print("Dataset loaded")
+
+    device = utils.get_device()
+    tokenizer = AutoTokenizer.from_pretrained(config.llama_model_name)
+
+    dataset = test_dataset.map(
+        lambda examples: decoder.tokenize_function(
+            tokenizer, examples, with_labels=False
+        ),
+        batched=True,
+    )
+
+    print("Loading model...")
+    model = AutoModelForCausalLM.from_pretrained(
+        utils.get_model_directory_name(config),
+        load_in_4bit=decoder.load_in_4bit(device),
+        device_map="auto",
+        torch_dtype=torch.float16,
+    )
+    print("Model loaded")
+    model = model.to(device)
+
+    distractors = decoder.generate_predictions(model, dataset, tokenizer, device)
+    predictions_dataset = utils.replace_distractors_in_dataset(
+        test_dataset, distractors
+    )
+    predictions_dataset.save_to_disk(utils.get_predictions_directory_name(config))
