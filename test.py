d360395dcf2f7d268bc7c506b9ef994ba98267b7
Oscar Zwagers <o.zwagers@icloud.com>
2025-04-06 16:20:54 +0200

    add bert lfs files

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 # small test to check if function and results are correct
+
+from datasets import load_from_disk
+from pprint import pprint
+
+
+dataset = load_from_disk("./project/predictions/mistral")
+
+pprint([row for row in dataset.select(range(30))])
